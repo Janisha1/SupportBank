905b259a2142d945c37da363e09893a61c85fa5e
RBletso <138777363+RBletso@users.noreply.github.com>
2023-08-09 14:32:49 +0100

    transaction class

---
+++ SupportBank-cs/Transactions.cs
@@ -0,0 +1,9 @@
+namespace SupportBank
+
+public class Transaction
+{
+    public DateTime Timestamp { get; set; }
+    public Person Payer { get; set; }
+    public Person Payee { get; set; }
+    public decimal Amount { get; set; }
+}
